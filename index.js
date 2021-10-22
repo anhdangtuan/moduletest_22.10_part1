2f220de591bb40f85775c1654b7fee21d698c88a
unknown <you@example.com>
2021-10-22 17:48:59 +0700

    update part 1

---
+++ index.js
@@ -0,0 +1,51 @@
+// Q37 print 3 and 4 to the screen
+// let i = 3;
+
+// for (; i < 5; i++) {
+//   console.log(i);
+// }
+// Q38
+// let i = 3;
+
+// for (; i < 5; ) {
+//   console.log(i);
+//   i += 1;
+
+// }
+// Q39
+// let i = 3;
+
+// for (;;) {
+//   console.log(i);
+//   i += 1;
+// }
+
+//Q40
+
+// for (i=0; i <= 10; i++) {
+//   console.log(i*i);
+// }
+
+const data = [
+    {
+      id: 1,
+      name: "A",
+      age: 20,
+    },
+    {
+      id: 2,
+      name: "B",
+      age: 21,
+    },
+    {
+      id: 3,
+      name: "C",
+      age: 22,
+    },
+  ];
+// Q41: Write a function with input (argument) is an array, output (return) is one element from input with age is 21. Test with above array.
+
+console.log(`age is ${data[1].age}`)
+
+// Q42.: Write a function with input (argument) is an array, converts this array into json format and saves to db.json file. Test with above array.
+
